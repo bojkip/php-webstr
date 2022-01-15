0d812a195b0050a91c00b7095b885258f8c31f19
Bojki <91488932+bojkip@users.noreply.github.com>
2022-01-15 10:35:38 +0100

    Add files via upload

---
+++ Laravel-bp/resources/views/sections/footer.blade.php
@@ -0,0 +1,22 @@
+<footer>
+  <section>
+    <span class="footer-span">About me</span>
+    <br><br>
+    Website creator
+  </section>
+
+  <section>
+    <span class="footer-span">Adress</span>
+    <br><br>
+    CA San Diego
+  </section>
+
+
+  <section>
+    <span class="footer-span">Information</span>
+    <br><br>
+    Info
+  </section>
+</footer>
+</body>
+</html>
