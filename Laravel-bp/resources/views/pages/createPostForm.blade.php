9c2fae5482eb26d4d1c2fd219c3b341e82717940
Bojki <91488932+bojkip@users.noreply.github.com>
2022-01-17 19:14:06 +0100

    Add files via upload

--- Laravel-bp/resources/views/pages/createPostForm.blade.php
+++ Laravel-bp/resources/views/pages/createPostForm.blade.php
@@ -5,33 +5,33 @@
 
 <div class="main-content">
 
-  <h2 style="text-align:center">Create a New Post</h2>
+<h2 style="text-align:center">Create a New Post</h2>
 
-  <p style="text-align:center">You create a new post by filling out this form</p>
+<p style="text-align:center">You create a new post by filling out this form</p>
 
-  <form action="{{ route('submitPostForm') }}" method="post" enctype="multipart/form-data">
-    @csrf
+<form action="{{ route('submitPostForm') }}" method="post" enctype="multipart/form-data">
+@csrf
 
-    <div class="input-block">
-      <label>Post Title</label>
-      <input type="text" id="post-title" name="post_title" placeholder="Please enter post title">
-    </div>
+<div class="input-block">
+    <label>Post Title</label>
+    <input type="text" id="post-title" name="post_title" placeholder="Please enter post title">
+</div>
 
-    <div class="input-block">
-    <label>Post Text</label>
-      <input type="text" id="post-text" name="post_text" placeholder="Please enter post text">
-    </div>
+<div class="input-block">
+<label>Post Text</label>
+    <input type="text" id="post-text" name="post_text" placeholder="Please enter post text">
+</div>
 
-    <div class="input-block">
-    <label>Post Image</label>
-      <input type="file" id="post-image" name="post_image">
-    </div>
+<div class="input-block">
+<label>Post Image</label>
+    <input type="file" id="post-image" name="post_image">
+</div>
 
-    <div class="input-block">
-      <input type="submit" value="Submit" id="post-submit-btn">
-    </div>
+<div class="input-block">
+    <input type="submit" value="Submit" id="post-submit-btn">
+</div>
 
-  </form>
+</form>
 
 </div>
 
