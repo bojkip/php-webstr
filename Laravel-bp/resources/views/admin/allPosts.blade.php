40eb4b24b41fc5c82492e13638e40bd25137cb1e
Bojki <91488932+bojkip@users.noreply.github.com>
2022-01-22 19:18:05 +0100

    Add files via upload

--- Laravel-bp/resources/views/admin/allPosts.blade.php
+++ Laravel-bp/resources/views/admin/allPosts.blade.php
@@ -21,30 +21,37 @@
   <div class="album py-5 bg-light">
     <div class="container">
 
+      @if(session('status'))
+
+      <h2 style="text-align: center; color:red">{{session('status')}}</h2>
+
+      @endif
+
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-      @foreach($posts as $post)
+
+        @foreach($posts as $post)
         <div class="col">
           <div class="card shadow-sm">
             <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"> -->
             <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="{{ asset('storage/posts_images/'.$post['image']) }}" alt="">
             <title>Placeholder</title>
             <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-          
+
 
             <div class="card-body">
               <p class="card-text">{{ $post['text'] }}</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  <a onclick="return confirm('Are you sure you want to delete this post?')" href="{{ route('deletePost', ['id'=>$post['id']]) }}" type="button" class="btn btn-sm btn-outline-danger me-1">Delete</a>
+                  <button type="button" class="btn btn-sm btn-secondary">Edit</button>
                 </div>
                 <small class="text-muted">9 mins</small>
               </div>
             </div>
           </div>
         </div>
-      @endforeach
+        @endforeach
 
         <!-- end row -->
       </div>
--- Laravel-bp/resources/views/admin/createPostForm.blade.php
+++ Laravel-bp/resources/views/admin/createPostForm.blade.php
@@ -3,50 +3,52 @@
 @section('adminContent')
 
 
-<div class="main-content">
+<div class="main-content pt-3">
 
-<h2 style="text-align:center">Create a New Post</h2>
+    <h2 style="text-align:center">Create a New Post</h2>
 
-<p style="text-align:center">You create a new post by filling out this form</p>
+    <p style="text-align:center">You create a new post by filling out this form</p>
 
-@if(session('status'))
+    @if(session('status'))
 
-<h2 style="text-align: center; color:pink">{{session('status')}}</h2>
+    <h2 style="text-align: center; color:red">{{session('status')}}</h2>
 
-@endif
+    @endif
 
-@if($errors->any())
+    @if($errors->any())
 
-@foreach($errors->all() as $error)
-<div style="text-align: center; color:pink;">{{$error}}</div>
-@endforeach
+    @foreach($errors->all() as $error)
+    <div style="text-align: center; color:red;">{{$error}}</div>
+    @endforeach
 
-@endif
+    @endif
 
 
-<form action="{{ route('submitPostForm') }}" method="post" enctype="multipart/form-data">
-@csrf
+    <form action="{{ route('submitPostForm') }}" method="post" enctype="multipart/form-data" class="container">
+        @csrf
 
-<div class="input-block">
-    <label>Post Title</label>
-    <input type="text" id="post-title" name="post_title" placeholder="Please enter post title">
-</div>
+        <div class="form-group">
+            <label>Post Title</label>
+            <input type="text" class="form-control" id="post-title" name="post_title" placeholder="Please enter post title">
+        </div>
 
-<div class="input-block">
-<label>Post Text</label>
-    <input type="text" id="post-text" name="post_text" placeholder="Please enter post text">
-</div>
+        <div class="form-group py-3">
+            <label>Post Text</label>
+            <input type="text" class="form-control" name="post_text" placeholder="Please enter post text">
+        </div>
 
-<div class="input-block">
-<label>Post Image</label>
-    <input type="file" id="post-image" name="post_image">
-</div>
+        <div class="form-group">
+            <label>Post Image</label>
+            <input type="file" class="form-control" id="post-image" name="post_image">
+        </div>
 
-<div class="input-block">
-    <input type="submit" value="Submit" id="post-submit-btn">
-</div>
+        <div class="form-group pt-3">
+            <div class="d-grid gap-2">
+                <input type="submit" value="Submit" class="btn btn-primary btn-lg" id="post-submit-btn">
+                </div>
+        </div>
 
-</form>
+    </form>
 
 </div>
 
