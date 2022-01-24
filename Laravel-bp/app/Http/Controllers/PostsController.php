3aaf4fdb29b7d156a4dc3222d633885dcdb0ab2a
Bojki <91488932+bojkip@users.noreply.github.com>
2022-01-24 19:03:56 +0100

    Add files via upload

--- Laravel-bp/app/Http/Controllers/PostsController.php
+++ Laravel-bp/app/Http/Controllers/PostsController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Post;
 
 class PostsController extends Controller
 {
+//
 
     public function index(){
 
-        $posts = Post::all();
+        $posts = DB::table('posts')->paginate(2);
         return view('pages.home',['posts'=>$posts]);
     }
 
