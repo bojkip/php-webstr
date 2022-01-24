c5c0c96c816381aadb18a65d6aadaf4805c90fa2
Bojki <91488932+bojkip@users.noreply.github.com>
2022-01-24 13:44:18 +0100

    Add files via upload

---
+++ Laravel-bp/resources/views/pages/ageRestriction.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+
+@section('content')
+
+<div class="main-content">
+
+<h1 class="text-align:center">Only people who are younger then 18 can access this page</h1>
+
+
+
+
+</div>
+
+@endsection
