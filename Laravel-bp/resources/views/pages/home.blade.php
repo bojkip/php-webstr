e13d12e638ac93aad1c8dcef332c88b0722fc22e
Bojki <91488932+bojkip@users.noreply.github.com>
2022-01-24 19:04:55 +0100

    Add files via upload

--- Laravel-bp/resources/views/pages/home.blade.php
+++ Laravel-bp/resources/views/pages/home.blade.php
@@ -7,19 +7,20 @@
   @foreach($posts as $post)
   <div class="single-post">
     <div class="post-text">
-      <h2> {{ $post['title'] }} </h2>
-      <p> {{ $post['text'] }} </p>
+      <h2> {{$post->title}} </h2>
+      <p> {{$post->text}} </p>
     </div>
 
     <div class="post-img">
-      <img width="250" height="250" src="{{ asset('storage/posts_images/'.$post['image']) }}">
+      <img width="250" height="250" src="{{ asset('storage/posts_images/'.$post->image) }}">
     </div>
 
   </div>
   @endforeach
 
-
-
+  <div>
+    {{$posts->links()}}
+  </div>
 
 </div>
 
