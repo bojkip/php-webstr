54b902a19cabc4bf65db8c7c6cb1b68e771a8b01
Bojki <91488932+bojkip@users.noreply.github.com>
2022-12-25 21:12:26 +0100

    post body

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\PostController;
 
 
 
@@ -13,11 +14,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+/*
 Route::get('/posts', function () {
     return view('posts.index');
 });
-
+*/
 Route::get('/home', function () {
     return view('home');
 })->name('home');;
@@ -32,6 +33,10 @@ Route::get('/register', [RegisterController::class , 'index'])->name('register')
 Route::post('/register', [RegisterController::class , 'store']);
 
 Route::get('/dashboard', [DashboardController::class , 'index'])->name('dashboard');
+
+Route::get('/posts', [PostController::class , 'index'])->name('posts');
+Route::post('/posts', [PostController::class , 'store']);
+
 
 
 
